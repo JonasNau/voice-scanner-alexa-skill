c323283c9ac6eaf307cabb6053855e910f6086ba
Jonas Naumann <jonasnaumann06@gmail.com>
2022-08-23 09:52:17 +0200

    changes

--- lambda/index.js
+++ lambda/index.js
@@ -145,7 +145,17 @@ const AllIntentHandler = {
   async handle(handlerInput) {
 
 
-    const speakOutput = "Test";
+    const speakOutput = {
+      "outputSpeech": {
+      "type": "SSML",
+      "ssml": `<speak>
+          Here is a number <w role='amazon:VBD'>read</w>
+          as a cardinal number:
+          <say-as interpret-as='cardinal'>12345</say-as>.
+          Here is a word spelled out:
+          <say-as interpret-as='spell-out'>hello</say-as>.</speak>`
+      }
+  }
 
     return (
       handlerInput.responseBuilder
