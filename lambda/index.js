0615b0ec12ac094503581bbad2b3d54ea6e16fea
Jonas Naumann <jonasnaumann06@gmail.com>
2022-08-29 23:06:52 +0200

    changes

--- lambda/index.js
+++ lambda/index.js
@@ -59,7 +59,7 @@ const LaunchRequestHandler = {
       .speak(speakOutput)
       .getResponse();
     } else {
-      let speakOutput = `${result.message} <audio src='https://eduquiz.ddns.net/weiteres/30s.mp3'/> Du kannst beispielsweise sagen: "seiteHinzufügen" oder "Hilfe". Möchtest du eine Seite hinzufügen?`;
+      let speakOutput = `${result.message} <audio src='https://v3.jovo.tech/audio/nf3ZAkyt-30s.mp3'/> Du kannst beispielsweise sagen: "seiteHinzufügen" oder "Hilfe". Möchtest du eine Seite hinzufügen?`;
       setState(handlerInput, "SeiteHinzufuegen")
       return handlerInput.responseBuilder
       .speak(speakOutput)
